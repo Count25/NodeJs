9f53d06e0def1fd42b2575722c60d9380c47a08a
Bruce <alejandrosanchezx@gmail.com>
2019-01-12 12:35:58 +0000

    first commit

---
+++ src/keys.js
@@ -0,0 +1,15 @@
+ //direzione del database
+
+ module.exports = {
+
+    
+
+     database:{
+         connectionLimit:10,
+         host:'localhost',
+         user: 'root',
+         password:'root',
+         database: 'database_links'
+     }
+ };
+ 
